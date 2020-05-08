fe40ee8f0f92f25a9fe88aac6191398d79ba5866
Adewoyin Oladipupo-Usoro <aolausoro@gmail.com>
2020-05-08 20:58:48 +0100

    Changed config.env

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,29 +3,10 @@ import React from "react";
 const Footer = () => {
   return (
     <footer>
-      <div className='bg-black block text-white bottom-0 left-0 mb-0 p-8'>
-        <div className='container text-center '>
-          <p className='z-50'>© 2019 Copyright Aolausoro.tech</p>
+      <div className="bg-black block text-white bottom-0 left-0 mb-0 p-6">
+        <div className="container text-center ">
+          <p className="z-50">© 2019 Copyright Aolausoro.tech</p>
         </div>
-        {/* <div className="">
-          <ul className="flex flex-row z-50">
-            <li className="px-1 py-2 m-1 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 hover:text-blue-500">
-              <a href="https://www.linkedin.com/in/adewoyin-oladipupo-usoro-267291100/">
-                <i className="fab fa-linkedin" />
-              </a>
-            </li>
-            <li className="px-1 py-2 m-1 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 hover:text-blue-500">
-              <a href="https://twitter.com/woy_in">
-                <i className="fab fa-twitter-square" />
-              </a>
-            </li>
-            <li className="px-1 py-2 m-1 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 hover:text-blue-500">
-              <a href="https://github.com/nucternal18">
-                <i className="fab fa-github" />
-              </a>
-            </li>
-          </ul>
-        </div> */}
       </div>
     </footer>
   );
